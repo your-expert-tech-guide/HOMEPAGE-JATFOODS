9be8b16fc359bb6377724bc1155d7c8bdbe75bf4
GRITO TALENT AGENCY <info@grito.africa>
2024-09-05 18:57:34 +0100

    01-Jatfoods-Home

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="">JATFOODS</a>
+      </header>
+      <main>OUR PRODUCTS</main>
+    </div>
+  );
+}
+
+export default App;
